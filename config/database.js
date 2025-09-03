d07b07b3dc59b858e1286acab228298914a0ce6b
kalpesh05 <kalpesh.relytree@gmail.com>
2025-09-03 11:54:19 +0530

    changes db

--- config/database.js
+++ config/database.js
@@ -4,15 +4,11 @@ require('dotenv').config();
 // Ensure database exists before creating pool
 async function ensureDatabaseExists() {
   console.log("::-----------------------", {
-    host: process.env.DB_HOST || process.env.MYSQLHOST,
-    port: process.env.DB_PORT || process.env.MYSQLPORT,
-    user: process.env.DB_USER || process.env.MYSQLUSER,
-    password: process.env.DB_PASSWORD || process.env.MYSQLPASSWORD,
-    database: process.env.DB_NAME || process.env.MYSQLDATABASE,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0,
-    connectTimeout: 60000,   // valid
+    host: process.env.MYSQL_HOST,       // <-- Railway-provided
+    port: process.env.MYSQL_PORT,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE
   })
   const connection = mysql.createConnection({
     host: process.env.DB_HOST || process.env.MYSQLHOST,
