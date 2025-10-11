ecb3e2ff5dbb19476c3fd6997856a9f813d6d783
unknown <kalpesh>
2025-10-11 12:13:36 +0530

    correction variable

--- utils/adsPlatformAuthenticator.js
+++ utils/adsPlatformAuthenticator.js
@@ -1380,7 +1380,7 @@ class AdPlatformAuthenticator {
           pageViews: parseInt(pageViews),
           users: parseInt(users),
           bounceRate: Number((bounceRate * 100).toFixed(2)),
-          avgSessionDuration:  Number((avgSessionDurationSec / 60).toFixed(2))
+          avgSessionDuration:  Number((avgSessionDurationSec))
         },
         message: "Success"
       };
